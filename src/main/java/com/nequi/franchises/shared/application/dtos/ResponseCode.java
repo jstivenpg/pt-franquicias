e98cd4a133767768ace9d783a59a56c07a65ec96
JStivenpgSGM <jstivenpachong@gmail.com>
2024-12-19 11:02:53 -0500

    feat: Response code done for franchises.

--- src/main/java/com/nequi/franchises/shared/application/dtos/ResponseCode.java
+++ src/main/java/com/nequi/franchises/shared/application/dtos/ResponseCode.java
@@ -12,8 +12,12 @@ public enum ResponseCode {
     // ERROR
     UNEXPECTED_ERROR("APP_GE_01", 500, "Unexpected error"),
     BAD_REQUEST("APP_GE_02", 400, "Bad request"),
+
     BRANCH_NOT_FOUND("APP_GE_03", 404, "Branch not found"),
-    BRANCH_NOT_DELETED("APP_GE_04", 400, "Branch not deleted");
+    BRANCH_NOT_DELETED("APP_GE_04", 400, "Branch not deleted"),
+
+    FRANCHISE_NOT_FOUND("APP_GE_04", 404, "Franchise not found"),
+    FRANCHISE_NOT_DELETED("APP_GE_05", 400, "Franchise not deleted");
 
     private final String code;
     private final int httpStatus;
